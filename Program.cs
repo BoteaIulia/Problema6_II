2c8c048d3fd95e63e8d64e6c5dac6acaa32744b2
dynamics <31436988+dynamics1@users.noreply.github.com>
2022-12-01 18:42:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public static class Globals
+{
+	internal static void Main()
+	{
+		int n;
+		int x;
+		int ok = 1;
+		int y;
+		n = Convert.ToInt32(Console.ReadLine());
+		x = Convert.ToInt32(Console.ReadLine());
+		for (int i = 1; i < n; i++)
+		{
+			y = Convert.ToInt32(Console.ReadLine());
+			if (x > y)
+			{
+				ok = 0;
+				x = y;
+			}
+			Console.Write("Secventa e in ordine crescatoare" + ok + " ");
+
+
+		}
+	}
+}
